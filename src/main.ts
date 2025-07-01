1c4ff17b522a180b49b6afbfdae2dc55579fa9f6
Daniel De la Torre <marco11.mendia@gmail.com>
2025-07-01 10:34:16 -0600

    remove debug console log from bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -23,8 +23,6 @@ async function bootstrap() {
   app.useGlobalFilters( new RpcCustomExceptionFilter() )
 
   await app.listen(envs.port);
-
-  console.log('Hola mundo - Primer cambio');
   
   logger.log(`Gateway running on port ${ envs.port }`)
 }
