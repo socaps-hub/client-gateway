51f81a4bbbbc485cb8e7cc46a04369f766803615
Daniel De la Torre <marco11.mendia@gmail.com>
2026-05-04 10:36:36 -0600

    Add operaciones ms

--- src/configuracion/credito/radiografia/radiografia.service.ts
+++ src/configuracion/credito/radiografia/radiografia.service.ts
@@ -1,39 +1,43 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config/services';
-import { CreateRadiografiaCargaArgs } from './dto/args/create-radiografia-carga.arg';
 import { radiografiasPatterns } from 'src/common/constants/radiografias/radiografiasPatterns';
-import { FileUpload } from 'graphql-upload-ts';
 import { RadioAreaEnum } from './enums/control-carga-radio-area.enum';
 import { Usuario } from 'src/configuracion/usuarios/entities/usuario.entity';
 
 @Injectable()
 export class RadiografiaService {
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
-    constructor(
-        @Inject(NATS_SERVICE) private readonly client: ClientProxy
-    ) { }
+  // crearCargaMasivaRadiografiaCredito( file: FileUpload, cooperativaId: string ) {
+  //     return this.client.send( radiografiasPatterns.UPLOAD_RA_CREDITO, { file, cooperativaId } );
+  // }
 
-    // crearCargaMasivaRadiografiaCredito( file: FileUpload, cooperativaId: string ) {        
-    //     return this.client.send( radiografiasPatterns.UPLOAD_RA_CREDITO, { file, cooperativaId } );
-    // }
+  getAllControlCargaRadiografias() {
+    return this.client.send(
+      radiografiasPatterns.GET_ALL_CONTROL_CARGA_RADIOS,
+      {},
+    );
+  }
 
-    getAllControlCargaRadiografias() {
-        return this.client.send( radiografiasPatterns.GET_ALL_CONTROL_CARGA_RADIOS, {} );
-    }
+  crearCargaMasivaRadiografiaCredito(key: string, cooperativaCodigo: string) {
+    return this.client.emit(radiografiasPatterns.UPLOAD_RA_CREDITO, {
+      key,
+      cooperativaCodigo,
+    });
+  }
 
-    crearCargaMasivaRadiografiaCredito(key: string, cooperativaCodigo: string) {
-        return this.client.emit(
-            radiografiasPatterns.UPLOAD_RA_CREDITO,
-            { key, cooperativaCodigo }
-        );
-    }
-
-    crearCargaMasivaRadiografia(key: string, cooperativaCodigo: string, area: RadioAreaEnum, user: Usuario) {
-        return this.client.emit(
-            radiografiasPatterns.UPLOAD_RADIOGRAFIA,
-            { key, cooperativaCodigo, area, user }
-        );
-    }
-
+  crearCargaMasivaRadiografia(
+    key: string,
+    cooperativaCodigo: string,
+    area: RadioAreaEnum,
+    user: Usuario,
+  ) {
+    return this.client.emit(radiografiasPatterns.UPLOAD_RADIOGRAFIA, {
+      key,
+      cooperativaCodigo,
+      area,
+      user,
+    });
+  }
 }
